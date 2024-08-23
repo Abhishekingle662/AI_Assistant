df9c8c9c300982f50fe7b6efa5807ade19c34a50
Abhishek Ingle <abhishekingle662@gmail.com>
2024-08-23 16:31:18 -0400

    formatting error correction

--- app/chat/page.js
+++ app/chat/page.js
@@ -77,7 +77,7 @@ export default function Home() {
             </ul>
           );
         } else {
-          return <p>{content}</p>;
+          return <span>{content}</span>;
         }
       };
 
